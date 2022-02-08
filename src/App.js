817de77fc8f978d4b4434db68e7b4a84909df48a
Shakhadat <shahodatbonu95@mail.ru>
2022-02-08 09:51:22 +0500

    add to git

--- src/App.js
+++ src/App.js
@@ -3,23 +3,159 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+<nav className="navbar navbar-expand-lg navbar-dark bg-dark my-navbar">
+  <div className="container">
+    <a className="navbar-brand" href="#">K. Griffith</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-2 mb-lg-0 text-right">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">APPEARANCES</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">BOOKS</a>
+        </li>
+       
+        <li className="nav-item">
+          <a className="nav-link">NEWS</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link">ABOUT</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link">CONTACT</a>
+        </li>
+      </ul>
+    
+     
+    </div>
+  </div>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-3">
+          <img src="./img/aftor.webp"  className="aftor_img" alt="vcc" />
+         
+        </div>
+        <div className="col-md-9">
+          <h1 className="headtit">Kayla Griffith</h1>
+          <span className="text-white subtitle">Award Winning Author</span>
+        </div>
+      </div>
+    </div>
+</nav>
+<div className="socNet">
+         
+         <ion-icon name="logo-twitter"></ion-icon>
+         <ion-icon name="logo-facebook"></ion-icon>
+</div>
+    <div className="container">
+      <div className="section2">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="subtit">New Release<div className="rec"></div></div>
+            <div className="textcontent">
+            <h1 className="text-white tit">The Swan Isle <br/>
+                  (2023)</h1>
+                <p className="text-white">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+            <h3 className="text-white ordr">Order Now</h3>
+            <ul className="btnContact">
+              <li><a href="#" className="btn btn-outline">Amazon</a></li>
+              <li><a href="#" className="btn">Google</a></li>
+              <li><a href="#" className="btn">ibooks</a></li>
+            </ul>
+            </div>
+           
+          </div>
+          <div className="col-md-6">
+            <div className="imgcontent">
+            <img src="./img/headbook.webp" alt="bgfd" />
+            </div>
+              
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="section3">
+      <div className="flagtop"><div className="rec2"></div></div>
+      <div className="container">
+        <h1 className="coltit py-5">Praise & Reviews</h1>
+
+        <div className="row">
+          <div className="col-md-4 px-5 text-center">
+            <h1 className="coltit">"</h1>
+            <p className="text-white text-center">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+            <p className="foopar">‘Swan Isle’ is Griffith's best writing yet” The Times Book Review</p>
+          </div>
+          <div className="col-md-4 px-5">
+          <h1 className="coltit">"</h1>
+            <p className="text-white text-center">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+            <p className="foopar">‘Gripping storytelling’
+The Good Read Blog</p>
+          </div>
+          <div className="col-md-4 px-5">
+          <h1 className="coltit">"</h1>
+            <p className="text-white text-center">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+            <p className="foopar">Grifith’s voice tells the story of all women “The Seattle Post Review”</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="section4 text-center">
+      <h1 className="coltit py-5">See Upcoming Appearances</h1>
+      <p className="text-white px-5">I'm a paragraph. Click here to add your own text and edit me. It's easy.</p>
+      <div className="row my-5">
+        <div className="col-md-2"></div>
+        <div className="col-md-8 p-5 cardmy">
+          <p className="text-white cardtext px-5">January 18th 2023, The Breakfast Club, Virtual Book Reading of Swan Isle, 7PM - 8PM EST</p>
+          <button className="btn btn-outline btnBor">Join</button>
+        </div>
+        <div className="col-md-2"></div>
+      </div>
+
+      <div className="row my-5">
+        <div className="col-md-2"></div>
+        <div className="col-md-8 p-5 cardmy">
+          <p className="text-white cardtext px-5">January 18th 2023, The Breakfast Club, Virtual Book Reading of Swan Isle, 7PM - 8PM EST</p>
+          <button className="btn btn-outline btnBor">Join</button>
+        </div>
+        <div className="col-md-2"></div>
+      </div>
+
+      <div className="row my-5 lastrow">
+        <div className="col-md-2"></div>
+        <div className="col-md-8 p-5 cardmy">
+          <p className="text-white cardtext px-5">January 18th 2023, The Breakfast Club, Virtual Book Reading of Swan Isle, 7PM - 8PM EST</p>
+          <button className="btn btn-outline btnBor">Join</button>
+        </div>
+        <div className="col-md-2"></div>
+      </div>
+
+    <div className="aboutKey">
+      <div className="imgbox">
+      <img src="./img/round.webp" alt="mnbvbn" />
+      </div>
+      
+      <div className="flagtop secondrec"><div className="rec2"></div></div>
+      <div className="aboutcontent">
+      <h1 className="coltit text-center">About Kayla Griffith</h1>
+    <p className="text-white text-center px-5">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+    <button className="btn btn-outline btnBor">Read More</button>
+      </div>
+  
+    </div>
+    <div className="footer">
+      <p className="text-center">© 2023 by K.Griffith. Proudly created with Wix.com</p>
+    </div>
+    </div>
+
+
     </div>
   );
-}
+   
+  }
 
 export default App;
